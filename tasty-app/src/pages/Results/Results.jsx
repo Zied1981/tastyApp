069a05541cca8497d3da78d3b4b451b04a032953
EvenSalomon1 <even.salo@web.de>
2024-03-08 15:53:16 +0100

    Add first part of Result page

--- tasty-app/src/pages/Results/Results.jsx
+++ tasty-app/src/pages/Results/Results.jsx
@@ -1,11 +1,40 @@
 import NavBar from "../../components/NavBar/NavBar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Results.css";
+
+import { useEffect, useState } from "react";
+
 const Results = () => {
+  // für Name Filter
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+      .then((res) => res.json())
+      .then((data) => setName(data))
+      .catch((err) => console.log("Fehler", err));
+  }, []);
+
+  // für Ingredient Filter
+  const [ingredient, setIngredient] = useState();
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+      .then((res) => res.json())
+      .then((data) => setIngredient(data))
+      .catch((err) => console.log("Fehler", err));
+  }, []);
+
+  console.log(ingredient);
+
   return (
     <section className="results">
       <SearchBar />
-      <h1>Results</h1>
+      {/* <section>
+        {name.map((item, index) => (
+          <div></div>
+        ))}
+      </section> */}
       <NavBar />
     </section>
   );
