007f7068c0dca1f825736b701ee91c0aaeb06299
Zied1981 <zkhemiri@gmail.com>
2024-03-08 15:59:48 +0100

    1push

--- tasty-app/src/pages/Details/Details.jsx
+++ tasty-app/src/pages/Details/Details.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import NavBar from "../../components/NavBar/NavBar";
+
 import "./Details.css";
 const Details = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772")
+      .then((res) => res.json())
+      .then((fetchData) => setData(fetchData))
+      .catch((err) => console.error("Fehler auf der deteilseite", err));
+  }, []);
+  console.log(data);
+
   return (
     <section className="details">
       <h1>Details</h1>
+      {/*     {data ? (
+        data.map((item, index) => (
+          <div key={index}>
+            <img src={item.strMealThumb} alt="image" />
+            <p>{item.strMeasure1}</p>
+            <p></p>
+            <p></p>
+          </div>
+        ))
+      ) : (
+        <p>loading...</p>
+      )} */}
+
       <NavBar />
     </section>
   );
