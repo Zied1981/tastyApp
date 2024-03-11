42e3e39cf2fe1ae9cd8019407fcc7340e3edfde5
Zied1981 <zkhemiri@gmail.com>
2024-03-11 09:17:36 +0100

    2 push

--- tasty-app/src/components/Instructions/Instructions.jsx
+++ tasty-app/src/components/Instructions/Instructions.jsx
@@ -1,9 +1,19 @@
 import "./Instructions.css";
 
-const Instructions = () => {
+const Instructions = (props) => {
+  console.log(props.toggolino);
   return (
-    <section className="instructions">
-      <h1>Instructions</h1>
+    <section
+      className={`instructions ${props.toggolino ? "toggleklasse" : ""}  `}
+    >
+      {props.instData ? (
+        <div>
+          <h3>Instructions</h3>
+          <p>{props.instData.strInstructions}</p>
+        </div>
+      ) : (
+        <p>Load...</p>
+      )}
     </section>
   );
 };
