46571d33d111ed56ff420ea3352b639db09468c6
EvenSalomon1 <even.salo@web.de>
2024-03-11 16:33:09 +0100

    update Results

--- tasty-app/src/pages/Results/Results.jsx
+++ tasty-app/src/pages/Results/Results.jsx
@@ -2,43 +2,65 @@ import NavBar from "../../components/NavBar/NavBar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Results.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+
+import { SearchContext } from "../../context/context";
 
 const Results = () => {
+  //  useContext für Search Input importieren
+  const { searchItem, setSearchItem } = useContext(SearchContext);
+
   // für Name Filter
   const [name, setName] = useState();
-
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-      .then((res) => res.json())
-      .then((data) => setName(data))
-      .catch((err) => console.log("Fehler", err));
-  }, []);
-
   // für Ingredient Filter
   const [ingredient, setIngredient] = useState();
 
+  // useEffect(() => {
+  //   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchItem}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setName(data))
+  //     .catch((err) => console.log("Fehler", err));
+  // }, [searchItem]);
+
   useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchItem}`)
       .then((res) => res.json())
       .then((data) => setIngredient(data))
       .catch((err) => console.log("Fehler", err));
   }, []);
 
-  console.log(ingredient);
+  console.log(name);
+  console.log(searchItem);
 
   return (
-    <section className="results">
+    <section>
       <SearchBar />
-      <section>
+      {/* <section className="results">
         {name ? (
-          name.map((item, index) => (
+          name.meals.map((item, index) => (
             <div key={index}>
               <img src={item.strMealThumb} alt="food" />
-              <div>
-                <h5>{item.strMeal}</h5>
-                <p>{item.strTags}</p>
-              </div>
+              <h5>{item.strMeal}</h5>
+              <img
+                src="../../../public/img/Arrow Right - Small.png"
+                alt="arrow"
+              />
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section> */}
+      <section>
+        {ingredient ? (
+          ingredient.meals.map((item, index) => (
+            <div key={index}>
+              <img src={item.strMealThumb} alt="food" />
+              <h5>{item.strMeal}</h5>
+              <img
+                src="../../../public/img/Arrow Right - Small.png"
+                alt="arrow"
+              />
             </div>
           ))
         ) : (
